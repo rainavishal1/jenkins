b92ff80c184bca1913ac1e95a067e8f100bab754
Vishal <rainavishal1@users.noreply.github.com>
2016-03-13 21:05:04 -0700

    Update pipeline.groovy

--- rdur/pipeline.groovy
+++ rdur/pipeline.groovy
@@ -32,7 +32,7 @@ node ('ubuntu-server') {
 
 //   sh "if [ -s ./mypid.txt ]; then kill -9 `cat ./mypid.txt`; fi; rm -f ./mypid.txt"
    unarchive mapping: ['war/target/jenkins.war' : '.']
-   unarchive mapping: ['rdur/deploy.sh' : '.']
+  // unarchive mapping: ['rdur/deploy.sh' : '.']
    sh "pwd"
   // sh "bash deploy.sh"
 //   sh "nohup java -jar jenkins.war &"
