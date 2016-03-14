7e6efe09e08d6cef107eb59af57834aa32c5600c
Vishal <rainavishal1@users.noreply.github.com>
2016-03-13 20:53:48 -0700

    back to basics

--- rdur/deploy.sh
+++ rdur/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x 
-ps -efwww|grep jenkins |grep -v grep|awk '{print $2}' > ./mypid.txt
+#ps -efwww|grep jenkins |grep -v grep|awk '{print $2}' > ./mypid.txt
 
 
-if [ -s ./mypid.txt ]; then 
-   kill -9 `cat ./mypid.txt`; 
-fi; 
+#if [ -s ./mypid.txt ]; then 
+#   kill -9 `cat ./mypid.txt`; 
+#fi; 
 
-rm -f ./mypid.txt
-java -jar jenkins.war &
+#rm -f ./mypid.txt
+java -jar ./jenkins.war 
 
